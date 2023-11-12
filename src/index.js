4029141f3165479790439d1d2e8aef228af37035
Niranjan <gawaliniranjan@gmail.com>
2023-11-12 10:59:20 +0530

    shopping application

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { ScrollToTop } from './components';
+import { CartProvider, FilterProvider } from './context';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <CartProvider>
+        <FilterProvider>
+          <ScrollToTop />
+          <ToastContainer
+            closeButton={true}
+            autoClose={3000}
+            position={'top-right'}
+          />
+          <App />
+        </FilterProvider>
+      </CartProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
